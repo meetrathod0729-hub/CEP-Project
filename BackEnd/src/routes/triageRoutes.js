e1b9adb144b2ab292df073bdcddcc9def09be408
Devansh270 <mehtadevansh27@gmail.com>
2026-04-23 19:31:30 +0530

    changes

--- BackEnd/src/routes/triageRoutes.js
+++ BackEnd/src/routes/triageRoutes.js
@@ -1,13 +1,3 @@
-// const express = require("express");
-
-// const router = express.Router();
-
-// router.get("/", (req, res) => {
-//   res.json({ message: "Triage route working" });
-// });
-
-// module.exports = router;
-
 const express = require("express");
 const router = express.Router();
 const { submitTriage, getAllTriage } = require("../controllers/triageController");
